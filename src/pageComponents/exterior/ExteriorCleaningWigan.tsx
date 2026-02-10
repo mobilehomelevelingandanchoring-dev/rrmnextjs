b63e810e414d24bd5bb3a954629697e2eeeb45ae
Asif Computers <developer@carkeysinstockport.com>
2026-02-09 23:46:19 -0800

    fix: add accessible title to Google Map iframe in ExteriorCleaningWigan
    - Add title='Map of Wigan and surrounding areas we service' to iframe element
    - Fixes accessibility error: 'Frames must have an accessible name'
    - Improves screen reader compatibility for all users

--- src/pageComponents/exterior/ExteriorCleaningWigan.tsx
+++ src/pageComponents/exterior/ExteriorCleaningWigan.tsx
@@ -289,6 +289,7 @@ export function ExteriorCleaningWigan() {
                   height="100%"
                   style={{ border: 0 }}
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d75038.58657181373!2d-2.628784!3d53.54639999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b1b1b8b8b8b8d%3A0x8b8b8b8b8b8b8b8b!2sWigan!5e0!3m2!1sen!2suk!4v1234567890123"
+                  title="Map of Wigan and surrounding areas we service"
                   allowFullScreen=""
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
